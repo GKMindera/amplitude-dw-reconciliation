879ce097180ef2f376c1614a23bae26f1a6f6b2f
GKMindera <71133807+GKMindera@users.noreply.github.com>
2026-04-01 14:41:59 -0500

    Add SQL query to count deposits in Amplitude table

---
+++ deposit/amp.sql
@@ -0,0 +1,9 @@
+  SELECT 
+    DATE(event_time) AS date,
+    COUNT(*) AS deposit_count
+  FROM foundation_views.application_events.amplitude_'<<PRODUCT>>' // e.g 'sportsbook'
+  WHERE event_time >= CURRENT_DATE()  - INTERVAL 30 DAY
+    AND event_time < CURRENT_DATE() 
+    AND event_type = 'Deposit'
+    AND event_properties['Product'] = '<<AMPLITUDE_PRODUCT>>' -- e.g 'sportsbook_cpe' 
+  GROUP BY DATE(event_time)
