8663cedca8f395a9eebe70e76834d1ad5edb4330
GKMindera <71133807+GKMindera@users.noreply.github.com>
2026-04-01 13:55:06 -0500

    Add SQL query for core user verification counts

---
+++ verification/dw.sql
@@ -0,0 +1,13 @@
+  SELECT 
+    DATE(verification_attempt_ts) AS date,
+    COUNT(DISTINCT(fanduel_user_id)) AS core_users
+  FROM foundation_views.account.verification_attempts
+  WHERE verification_attempt_ts >= CURRENT_DATE() - INTERVAL 30 DAY
+    AND verification_attempt_ts < CURRENT_DATE()
+    AND verification_status_after_attempt = 1
+    AND verification_type = 2
+    AND performer_id != 1
+    AND is_under_age IS FALSE
+    AND is_ineligible_state IS FALSE
+    AND product_name = 'SB'
+  GROUP BY 1
