a2ccb85006f461d3461c968d571c8e79b267f8bf
GKMindera <71133807+GKMindera@users.noreply.github.com>
2026-04-01 13:57:51 -0500

    Change product_name to a placeholder in SQL query
    Updated product_name condition to use a placeholder.

--- verification/dw.sql
+++ verification/dw.sql
@@ -9,5 +9,5 @@
     AND performer_id != 1
     AND is_under_age IS FALSE
     AND is_ineligible_state IS FALSE
-    AND product_name = 'SB'
+    AND product_name = '<<PRODUCT_NAME>>' -- add product_name here e.g 'sportsbook'
   GROUP BY 1
