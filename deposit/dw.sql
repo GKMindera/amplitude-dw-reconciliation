87df76648090bd0634f0fbc8231b1d730cc2c9fb
George Stephen <71133807+GKMindera@users.noreply.github.com>
2026-04-02 11:34:30 -0500

    Correct SQL comment syntax in dw.sql
    Fix SQL syntax for deposit_product_name condition.

--- deposit/dw.sql
+++ deposit/dw.sql
@@ -4,6 +4,6 @@
   FROM core_views.financial.deposits
   WHERE deposit_complete_ts >= CURRENT_DATE()  - INTERVAL 30 DAY
     AND deposit_complete_ts < CURRENT_DATE() 
-    AND deposit_product_name = '<<DEPOSIT_PRODUCT_NAME>>" -- e.g 'sportsbook online'
+    AND deposit_product_name = '<<DEPOSIT_PRODUCT_NAME>>' -- e.g 'sportsbook online'
     AND deposit_status = 'completed'
   GROUP BY DATE(deposit_complete_ts)
